069eec659f3e42c21a68f69031a5713e9a7e094e
twenk11k <selimdemiral@hotmail.com>
2019-04-17 17:29:36 +0300

    Fix expected opening time input bug and other minor fixes

--- js/kaza_bildiri.js
+++
@@ -1,8 +0,0 @@
-$('#idTourDateDetails').datepicker({
-    dateFormat: 'dd-mm-yy',
-    minDate: '+5d',
-    changeMonth: true,
-    changeYear: true,
-    altField: "#idTourDateDetailsHidden",
-    altFormat: "yy-mm-dd"
-});
